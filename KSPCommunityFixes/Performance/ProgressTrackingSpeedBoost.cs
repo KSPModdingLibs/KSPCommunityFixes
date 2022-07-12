e0402c3cbec98c8752456adbbb932609dcca2608
JonnyOThan <jonnyothan@gmail.com>
2022-07-12 14:38:12 -0400

    Fix #55: Add ProgressTrackingSpeedBoost
    KSP's stock ProgressTracking evaluates every progress node for every vessel in the universe.
    There are ~19 progress nodes per celestial body, and this can get extremely expensive for mods with a
    large number of bodies or saves with a large number of vessels.
    This tweak only evaluates the currently active vessel and removes the per-body checks which don't
    have any effect, at least in the stock game.

---
+++ KSPCommunityFixes/Performance/ProgressTrackingSpeedBoost.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace KSPCommunityFixes.Performance
+{
+	public class ProgressTrackingSpeedBoost : BasePatch
+	{
+		protected override void ApplyPatches(List<PatchInfo> patches)
+		{
+			patches.Add(new PatchInfo(
+				PatchMethodType.Prefix,
+				AccessTools.Method(typeof(ProgressTracking), nameof(ProgressTracking.Update)),
+				this));
+
+			patches.Add(new PatchInfo(
+				PatchMethodType.Postfix,
+				AccessTools.DeclaredConstructor(typeof(KSPAchievements.CelestialBodySubtree), new Type[] { typeof(CelestialBody) }),
+				this,
+				nameof(CelestialBodySubtree_Constructor_Postfix)));
+		}
+
+		static bool ProgressTracking_Update_Prefix(ProgressTracking __instance)
+		{
+			if (!HighLogic.LoadedSceneIsEditor && FlightGlobals.ActiveVessel != null)
+			{
+				__instance.achievementTree.IterateVessels(FlightGlobals.ActiveVessel);
+			}
+
+			return false;
+		}
+
+		static void CelestialBodySubtree_Constructor_Postfix(KSPAchievements.CelestialBodySubtree __instance)
+		{
+			__instance.OnIterateVessels = null;
+		}
+	}
+}
