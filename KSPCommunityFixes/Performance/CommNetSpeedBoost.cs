c1ce01c7288b78a627828ace7e5efd14e77170d9
JonnyOThan <jonnyothan@gmail.com>
2022-07-13 13:20:32 -0400

    Fix #56: Fix CommnetNetwork.Update performance The throttling logic that was supposed to update it once every 5 seconds was broken

---
+++ KSPCommunityFixes/Performance/CommNetSpeedBoost.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSPCommunityFixes.Performance
+{
+	class CommNetSpeedBoost : BasePatch
+	{
+		private static double packedInterval = 0.5;
+		private static double unpackedInterval = 5.0;
+
+		protected override void ApplyPatches(List<PatchInfo> patches)
+		{
+			ConfigNode settingsNode = KSPCommunityFixes.SettingsNode.GetNode("COMM_NET_PERFORMANCE_SETTINGS");
+
+			if (settingsNode != null)
+			{
+				settingsNode.TryGetValue("packedInterval", ref packedInterval);
+				settingsNode.TryGetValue("unpackedInterval", ref unpackedInterval);
+			}
+
+			patches.Add(new PatchInfo(
+				PatchMethodType.Prefix,
+				AccessTools.Method(typeof(CommNet.CommNetNetwork), nameof(CommNet.CommNetNetwork.Update)),
+				this));
+		}
+
+		static bool CommNetNetwork_Update_Prefix(CommNet.CommNetNetwork __instance)
+		{
+			double timeSinceLastUpdate = Time.timeSinceLevelLoad - __instance.prevUpdate;
+
+			if (FlightGlobals.ActiveVessel != null)
+			{
+				double interval = FlightGlobals.ActiveVessel.packed ? packedInterval : unpackedInterval;
+				if (timeSinceLastUpdate < interval)
+				{
+					__instance.graphDirty = true;
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
