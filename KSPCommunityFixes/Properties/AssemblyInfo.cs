af1dfc5d7958488e8b2117a7a4720fa97ad46d6e
NathanKell <nathankell@gmail.com>
2022-08-12 00:38:11 -0700

    Prepare for Release 1.20.4

--- KSPCommunityFixes/Properties/AssemblyInfo.cs
+++ KSPCommunityFixes/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.20.3.0")]
-[assembly: AssemblyFileVersion("1.20.3.0")]
+[assembly: AssemblyVersion("1.20.4.0")]
+[assembly: AssemblyFileVersion("1.20.4.0")]
