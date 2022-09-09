af6ea83268a506f2b39aa04fb5da44edc6c3faa8
NathanKell <nathankell@gmail.com>
2022-09-08 22:14:25 -0700

    Fix missing attribute for MultipleModulePartAPI, refactor WriteValue for better support for other mods, update release script to behave correctly with new versioning bump version (only fileversion and KSPAssembly).

--- KSPCommunityFixes/Properties/AssemblyInfo.cs
+++ KSPCommunityFixes/Properties/AssemblyInfo.cs
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.22.0.0")]
-[assembly: AssemblyFileVersion("1.22.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.22.1.0")]
 
-[assembly: KSPAssembly("KSPCommunityFixes", 1, 22, 0)]
+[assembly: KSPAssembly("KSPCommunityFixes", 1, 22, 1)]
+[assembly: KSPAssemblyDependency("MultipleModulePartAPI", 1, 0, 0)]
