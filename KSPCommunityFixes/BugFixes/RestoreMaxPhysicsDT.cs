adfb16df5eb39893d4988442b9271b803b40d498
JonnyOThan <jonnyothan@gmail.com>
2022-09-13 16:19:46 -0400

    Add RestoreMaxPhysicsDT When using physics warp, Unity will set the max physics dt to be at least as high as the scaled physics dt.  But KSP will never restore it back to the normal value from the settings.  This can degrade performance as it allows more FixedUpdates to run per frame.
    reported by Wilds on the forum: https://forum.kerbalspaceprogram.com/index.php?/topic/184740-solved-losing-300-performance-after-time-warping/

---
+++ KSPCommunityFixes/BugFixes/RestoreMaxPhysicsDT.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSPCommunityFixes.BugFixes
+{
+    public class RestoreMaxPhysicsDT : BasePatch
+    {
+        protected override void ApplyPatches(List<PatchInfo> patches)
+        {
+            patches.Add(new PatchInfo(
+                PatchMethodType.Prefix,
+                AccessTools.Method(typeof(TimeWarp), "updateRate"),
+                this));
+        }
+
+        static void TimeWarp_updateRate_Prefix()
+        {
+            // when you use physics warp, it increases Time.fixedDeltaTime
+            // Unity will internally increase maximumDeltaTime to be at least as high as fixedDeltaTime
+            // But nothing in the KSP code will ever return maximumDeltaTime to the value from the settings when time warp is over
+            // Setting it just before TimeWarp sets fixedDeltaTime guarantees that all points of code will have the correct value
+            Time.maximumDeltaTime = GameSettings.PHYSICS_FRAME_DT_LIMIT;
+        }
+    }
+}
