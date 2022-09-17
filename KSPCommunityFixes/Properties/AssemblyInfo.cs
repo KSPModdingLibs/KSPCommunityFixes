8bce17745019bb664c6393bb18fc9be3e3c20f44
NathanKell <nathankell@gmail.com>
2022-09-16 23:55:22 -0700

    Bump version, add readme info for RestoreMaxPhysicsDT

--- KSPCommunityFixes/Properties/AssemblyInfo.cs
+++ KSPCommunityFixes/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.22.2.0")]
+[assembly: AssemblyFileVersion("1.23.0.0")]
 
-[assembly: KSPAssembly("KSPCommunityFixes", 1, 22, 2)]
+[assembly: KSPAssembly("KSPCommunityFixes", 1, 23, 0)]
 [assembly: KSPAssemblyDependency("MultipleModulePartAPI", 1, 0, 0)]
