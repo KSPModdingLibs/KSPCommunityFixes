94cf2bcd45dbd6869e70398d7d39fbaf14089ab6
gotmachine <24925209+gotmachine@users.noreply.github.com>
2022-12-03 19:25:43 +0100

    Implemented GameDatabase loader frame unlocking

--- KSPCommunityFixes/Performance/FPSUnlockedLoading.cs
+++ KSPCommunityFixes/Performance/FPSUnlockedLoading.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -9,13 +10,15 @@ using System.Threading;
 using System.Threading.Tasks;
 using HarmonyLib;
 using UnityEngine;
+using static GameDatabase;
+using static UrlDir;
 
 namespace KSPCommunityFixes.Performance
 {
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class FPSUnlockedLoading : MonoBehaviour
     {
-        private static float minFrameTime = 1f / 60f;
+        private static float minFrameTime = 1f / 30f;
 
         private static Harmony harmony;
 
@@ -23,6 +26,15 @@ namespace KSPCommunityFixes.Performance
         {
             harmony = new Harmony(nameof(FPSUnlockedLoading));
             Harmony.DEBUG = true;
+
+            MethodInfo m_GameDatabase_SetupMainLoaders = AccessTools.Method(typeof(GameDatabase), nameof(GameDatabase.SetupMainLoaders));
+            MethodInfo t_GameDatabase_SetupMainLoaders = AccessTools.Method(typeof(FPSUnlockedLoading), nameof(GameDatabase_SetupMainLoaders_Postfix));
+            harmony.Patch(m_GameDatabase_SetupMainLoaders, null, new HarmonyMethod(t_GameDatabase_SetupMainLoaders));
+
+            // GameDatabase_LoadAssetBundleObjects_MoveNext_Prefix
+            MethodInfo m_GameDatabase_LoadAssetBundleObjects_MoveNext = AccessTools.EnumeratorMoveNext(AccessTools.Method(typeof(GameDatabase), nameof(GameDatabase.LoadAssetBundleObjects)));
+            MethodInfo t_GameDatabase_LoadAssetBundleObjects_MoveNext = AccessTools.Method(typeof(FPSUnlockedLoading), nameof(GameDatabase_LoadAssetBundleObjects_MoveNext_Prefix));
+            harmony.Patch(m_GameDatabase_LoadAssetBundleObjects_MoveNext, new HarmonyMethod(t_GameDatabase_LoadAssetBundleObjects_MoveNext));
 
             MethodInfo m_PartLoader_StartLoad = AccessTools.Method(typeof(PartLoader), nameof(PartLoader.StartLoad));
             MethodInfo t_PartLoader_StartLoad = AccessTools.Method(typeof(FPSUnlockedLoading), nameof(PartLoader_StartLoad_Transpiler));
@@ -36,8 +48,6 @@ namespace KSPCommunityFixes.Performance
         IEnumerator Start()
         {
             yield return null;
-
-
         }
 
         static void PatchStartCoroutineInEnumerator(MethodInfo enumerator)
@@ -100,6 +110,207 @@ namespace KSPCommunityFixes.Performance
 
             return code;
         }
+
+        static void GameDatabase_SetupMainLoaders_Postfix(GameDatabase __instance, List<UrlDir.ConfigFileType> __result)
+        {
+            __instance.StartCoroutine(GameDatabase_LoadObjects(__result));
+            loadObjectsInProgress = true;
+            throw new Exception("Terminating stock GameDatabase.LoadObjects() coroutine");
+        }
+
+        static bool loadObjectsInProgress = false;
+
+        static IEnumerator GameDatabase_LoadObjects(List<UrlDir.ConfigFileType> configFileTypes)
+        {
+            Debug.Log("[KSPCF] Starting custom GameDatabase_LoadObjects()");
+            GameDatabase instance = GameDatabase.Instance;
+
+            instance._root = new UrlDir(instance.urlConfig.ToArray(), configFileTypes.ToArray());
+            instance.translateLoadedNodes();
+            int audioCount = 0;
+            int textureCount = 0;
+            int modelCount = 0;
+            foreach (UrlDir.UrlFile audioFile in instance.root.GetFiles(UrlDir.FileType.Audio))
+            {
+                if (audioFile != null)
+                {
+                    audioCount++;
+                }
+            }
+            foreach (UrlDir.UrlFile textureFile in instance.root.GetFiles(UrlDir.FileType.Texture))
+            {
+                if (textureFile != null)
+                {
+                    textureCount++;
+                }
+            }
+            foreach (UrlDir.UrlFile modelFile in instance.root.GetFiles(UrlDir.FileType.Model))
+            {
+                if (modelFile != null)
+                {
+                    modelCount++;
+                }
+            }
+            float delta = 1f / (float)(audioCount + textureCount + modelCount);
+            instance.progressFraction = 0f;
+            bool loadTextures = true;
+            bool loadAudio = true;
+            bool loadParts = true;
+            float nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
+            foreach (UrlDir.UrlFile audioFile in instance.root.GetFiles(UrlDir.FileType.Audio))
+            {
+                if (instance.ExistsAudioClip(audioFile.url))
+                {
+                    if (!(audioFile.fileTime > instance.lastLoadTime))
+                    {
+                        continue;
+                    }
+                    UnityEngine.Debug.Log("Load(Audio): " + audioFile.url + " OUT OF DATE");
+                    instance.RemoveAudioClip(audioFile.url);
+                    PartLoader.Instance.Recompile = true;
+                }
+                instance.progressTitle = audioFile.url;
+                UnityEngine.Debug.Log("Load(Audio): " + audioFile.url);
+                foreach (DatabaseLoader<AudioClip> loader3 in instance.loadersAudio)
+                {
+                    if (loader3.extensions.Contains(audioFile.fileExtension))
+                    {
+                        if (loadAudio)
+                        {
+                            IEnumerator loaderEnumerator = loader3.Load(audioFile, new FileInfo(audioFile.fullPath));
+                            while (loaderEnumerator.MoveNext())
+                            {
+                                if (Time.realtimeSinceStartup > nextFrameTime)
+                                {
+                                    nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
+                                    yield return null;
+                                }
+                            }
+                        }
+                        if (loader3.successful)
+                        {
+                            loader3.obj.name = audioFile.url;
+                            instance.databaseAudio.Add(loader3.obj);
+                            instance.databaseAudioFiles.Add(audioFile);
+                        }
+                    }
+                }
+                instance.progressFraction += delta;
+            }
+            foreach (UrlDir.UrlFile file3 in instance.root.GetFiles(UrlDir.FileType.Texture))
+            {
+                if (instance.ExistsTexture(file3.url))
+                {
+                    if (!(file3.fileTime > instance.lastLoadTime))
+                    {
+                        continue;
+                    }
+                    UnityEngine.Debug.Log("Load(Texture): " + file3.url + " OUT OF DATE");
+                    instance.RemoveTexture(file3.url);
+                    PartLoader.Instance.Recompile = true;
+                }
+                instance.progressTitle = file3.url;
+                UnityEngine.Debug.Log("Load(Texture): " + file3.url);
+                foreach (DatabaseLoader<TextureInfo> loader2 in instance.loadersTexture)
+                {
+                    if (loader2.extensions.Contains(file3.fileExtension))
+                    {
+                        instance._recompileModels = true;
+                        if (loadTextures)
+                        {
+                            IEnumerator loaderEnumerator = loader2.Load(file3, new FileInfo(file3.fullPath));
+                            while (loaderEnumerator.MoveNext())
+                            {
+                                if (Time.realtimeSinceStartup > nextFrameTime)
+                                {
+                                    nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
+                                    yield return null;
+                                }
+                            }
+                        }
+                        if (loader2.successful)
+                        {
+                            loader2.obj.name = file3.url;
+                            loader2.obj.texture.name = file3.url;
+                            instance.databaseTexture.Add(loader2.obj);
+                        }
+                    }
+                }
+                instance.progressFraction += delta;
+            }
+            if (instance._recompileModels)
+            {
+                foreach (GameObject item in instance.databaseModel)
+                {
+                    UnityEngine.Object.Destroy(item.gameObject);
+                }
+                instance.databaseModel.Clear();
+            }
+            foreach (UrlDir.UrlFile file3 in instance.root.GetFiles(UrlDir.FileType.Model))
+            {
+                if (instance.ExistsModel(file3.url))
+                {
+                    if (!(file3.fileTime > instance.lastLoadTime))
+                    {
+                        continue;
+                    }
+                    UnityEngine.Debug.Log("Load(Model): " + file3.url + " OUT OF DATE");
+                    instance.RemoveModel(file3.url);
+                    PartLoader.Instance.Recompile = true;
+                }
+                instance.progressTitle = file3.url;
+                UnityEngine.Debug.Log("Load(Model): " + file3.url);
+                foreach (DatabaseLoader<GameObject> loader in instance.loadersModel)
+                {
+                    if (loader.extensions.Contains(file3.fileExtension))
+                    {
+                        if (loadParts)
+                        {
+                            IEnumerator loaderEnumerator = loader.Load(file3, new FileInfo(file3.fullPath));
+                            while (loaderEnumerator.MoveNext())
+                            {
+                                if (Time.realtimeSinceStartup > nextFrameTime)
+                                {
+                                    nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
+                                    yield return null;
+                                }
+                            }
+                        }
+                        if (loader.successful)
+                        {
+                            GameObject obj = loader.obj;
+                            obj.transform.name = file3.url;
+                            obj.transform.parent = instance.transform;
+                            obj.transform.localPosition = Vector3.zero;
+                            obj.transform.localRotation = Quaternion.identity;
+                            obj.SetActive(value: false);
+                            instance.databaseModel.Add(obj);
+                            instance.databaseModelFiles.Add(file3);
+                        }
+                    }
+                }
+                instance.progressFraction += delta;
+            }
+            instance.lastLoadTime = KSPUtil.SystemDateTime.DateTimeNow();
+            instance.progressFraction = 1f;
+            loadObjectsInProgress = false;
+
+            Debug.Log("[KSPCF] Finished custom GameDatabase_LoadObjects()");
+        }
+
+        static bool GameDatabase_LoadAssetBundleObjects_MoveNext_Prefix(object __instance, ref bool __result)
+        {
+            if (loadObjectsInProgress)
+            {
+                FieldInfo f_current = AccessTools.GetDeclaredFields(__instance.GetType()).First(p => p.Name.Contains("current"));
+                f_current.SetValue(__instance, null);
+                __result = true;
+                return false;
+            }
+
+            return true;
+        }
+
 
         static IEnumerator PartLoader_CompileAll()
         {
@@ -168,51 +379,6 @@ namespace KSPCommunityFixes.Performance
             while (compileInternalSpacesEnumerator.MoveNext())
                 yield return null;
 
-
-            // float nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
-
-            //IEnumerator compilePartsEnumerator = instance.CompileParts(configs);
-            //while (compilePartsEnumerator.MoveNext())
-            //{
-            //    if (Time.realtimeSinceStartup > nextFrameTime)
-            //    {
-            //        nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
-            //        yield return null;
-            //    }
-            //    else
-            //    {
-            //        Thread.Sleep(0);
-            //    }
-            //}
-
-            //IEnumerator compileInternalPropsEnumerator = instance.CompileInternalProps(allPropNodes);
-            //while (compileInternalPropsEnumerator.MoveNext())
-            //{
-            //    if (Time.realtimeSinceStartup > nextFrameTime)
-            //    {
-            //        nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
-            //        yield return null;
-            //    }
-            //    else
-            //    {
-            //        Thread.Sleep(0);
-            //    }
-            //}
-
-            //IEnumerator compileInternalSpacesEnumerator = instance.CompileInternalSpaces(allSpaceNodes);
-            //while (compileInternalSpacesEnumerator.MoveNext())
-            //{
-            //    if (Time.realtimeSinceStartup > nextFrameTime)
-            //    {
-            //        nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
-            //        yield return null;
-            //    }
-            //    else
-            //    {
-            //        Thread.Sleep(0);
-            //    }
-            //}
-
             instance.SavePartDatabase();
             instance._recompile = false;
             PartUpgradeManager.Handler.LinkUpgrades();
@@ -232,21 +398,17 @@ namespace KSPCommunityFixes.Performance
             {
                 while (currentEnumerator.MoveNext())
                 {
-                    if (currentEnumerator.Current is IEnumerator nestedCoroutine)
-                    {
-                        enumerators.Push(currentEnumerator);
-                        currentEnumerator = nestedCoroutine;
-                        continue;
-                    }
-
                     if (Time.realtimeSinceStartup > nextFrameTime)
                     {
                         nextFrameTime = Time.realtimeSinceStartup + minFrameTime;
                         yield return null;
                     }
-                    else
+
+                    if (currentEnumerator.Current is IEnumerator nestedCoroutine)
                     {
-                        Thread.Sleep(0);
+                        enumerators.Push(currentEnumerator);
+                        currentEnumerator = nestedCoroutine;
+                        continue;
                     }
                 }
             }
