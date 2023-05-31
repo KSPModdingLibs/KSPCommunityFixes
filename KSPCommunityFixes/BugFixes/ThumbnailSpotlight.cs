73d398c381080aa0562dd3308cf803a931843724
JonnyOThan <jonnyothan@gmail.com>
2023-05-31 18:04:38 -0400

    Fix #148: clean up camera and spotlight objects when a part thumbnail fails to be generated (#149)

---
+++ KSPCommunityFixes/BugFixes/ThumbnailSpotlight.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSPCommunityFixes
+{
+    class ThumbnailSpotlight : BasePatch
+    {
+        protected override Version VersionMin => new Version(1, 12, 0);
+
+		protected override void ApplyPatches(List<PatchInfo> patches)
+		{
+			patches.Add(new PatchInfo(
+				PatchMethodType.Postfix,
+				AccessTools.Method(typeof(CraftThumbnail), "TakePartSnapshot"),
+				this));
+		}
+
+		private static void CraftThumbnail_TakePartSnapshot_Postfix()
+		{
+			if (CraftThumbnail.snapshotCamera != null)
+			{
+				UnityEngine.Object.Destroy(CraftThumbnail.snapshotCamera.gameObject);
+			}
+		}
+	}
+}
