f6f4c63830d2293c34e759b85e28ee9aab39dcf7
gotmachine <24925209+gotmachine@users.noreply.github.com>
2024-09-24 20:44:14 +0200

    Many experimental perf improvements, basically a big collection of micro-optimizations for various pieces of code running every frame on every part. Won't do much in low part count situations, and some patches are highly situational (atmospheric flight, docking ports, struts, particles repositionning on floating origin shifts...) Patches affecting the FlightIntegrator and VesselPrecalculate are broably incompatible with mods trying to override/extend stuff through MFI. There are a bunch of perf issues with "end of the call stack" methods being inefficient because they do extra work that is already done at the call site or can be made faster by using state from the call site, so patching those methods doesn't cut it, but this mean many stock methods aren't called anymore, so if those methods are replaced/prefixed/postfixed with MFI, things will break. Notable examples are FI.UpdateAerodynamics() and FI.Integrate(Part). This could probably be avoided at the cost of some of the perf gains, but I wanted to see how far things could be pushed perf wise. On a side note, there is a bug lurking in the VesselPrecalculate.CalculatePhysicsStats reimplementation, not sure what it is exactely but a side effect is camera target being wrongly offset. Likely something with the CoM calcs.

--- KSPCommunityFixes/Library/Numerics.cs
+++ KSPCommunityFixes/Library/Numerics.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using Random = System.Random;
 
 namespace KSPCommunityFixes.Library
 {
     internal static class Numerics
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Zero(ref this Vector3 v)
+        {
+            v.x = 0f; 
+            v.y = 0f; 
+            v.z = 0f;
+        }
+
         /// <summary>
         /// mutating add, ~10 times faster than using the + operator
         /// </summary>
@@ -15,6 +25,135 @@ namespace KSPCommunityFixes.Library
             v.x += other.x;
             v.y += other.y;
             v.z += other.z;
+        }
+
+        /// <summary>
+        /// mutating add, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Add(ref this Vector3d v, double x, double y, double z)
+        {
+            v.x += x;
+            v.y += y;
+            v.z += z;
+        }
+
+        /// <summary>
+        /// mutating add, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Add(ref this Vector3 v, Vector3 other)
+        {
+            v.x += other.x;
+            v.y += other.y;
+            v.z += other.z;
+        }
+
+        /// <summary>
+        /// mutating add, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Add(ref this Vector3 v, float x, float y, float z)
+        {
+            v.x += x;
+            v.y += y;
+            v.z += z;
+        }
+
+        /// <summary>
+        /// mutating substract, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Substract(ref this Vector3d v, Vector3d other)
+        {
+            v.x -= other.x;
+            v.y -= other.y;
+            v.z -= other.z;
+        }
+
+        /// <summary>
+        /// mutating substract, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Substract(ref this Vector3d v, double x, double y, double z)
+        {
+            v.x -= x;
+            v.y -= y;
+            v.z -= z;
+        }
+
+        /// <summary>
+        /// mutating substract, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Substract(ref this Vector3 v, Vector3 other)
+        {
+            v.x -= other.x;
+            v.y -= other.y;
+            v.z -= other.z;
+        }
+
+        /// <summary>
+        /// mutating substract, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Substract(ref this Vector3 v, float x, float y, float z)
+        {
+            v.x -= x;
+            v.y -= y;
+            v.z -= z;
+        }
+
+        /// <summary>
+        /// mutating multiply, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Multiply(ref this Vector3d v, double scalar)
+        {
+            v.x *= scalar;
+            v.y *= scalar;
+            v.z *= scalar;
+        }
+
+        /// <summary>
+        /// mutating multiply, ~10 times faster than using the + operator
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Multiply(ref this Vector3 v, float scalar)
+        {
+            v.x *= scalar;
+            v.y *= scalar;
+            v.z *= scalar;
+        }
+
+        /// <summary>
+        /// Clamp a value between 0.0 and 1.0
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Clamp01(double value)
+        {
+            if (value < 0.0)
+                return 0.0;
+
+            if (value > 1.0)
+                return 1.0;
+
+            return value;
+        }
+
+        /// <summary>
+        /// Double backed clamped lerp
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Lerp(double a, double b, double t)
+        {
+            if (t <= 0f)
+                return a;
+
+            if (t >= 1f)
+                return a + (b - a);
+
+            return a + (b - a) * t;
         }
 
         /// <summary>
@@ -111,5 +250,187 @@ namespace KSPCommunityFixes.Library
         /// Evaluate whether a given integral value is a power of 2.
         /// </summary>
         public static bool IsPowerOfTwo(uint value) => (value & (value - 1)) == 0 && value != 0;
+
+        /// <summary>
+        /// Faster approximate alternative to Math.Pow(), by a factor 5-6. Max error around 5%, average error around 1.7%
+        /// </summary>
+        public static unsafe double FastPow(double value, double exponent)
+        {
+            // exponentiation by squaring
+            double r = 1.0;
+            int exp = (int)exponent;
+            double _base = value;
+            while (exp != 0)
+            {
+                if ((exp & 1) != 0)
+                {
+                    r *= _base;
+                }
+                _base *= _base;
+                exp >>= 1;
+            }
+
+            // use the IEEE 754 trick for the fraction of the exponent
+            double b_faction = exponent - (int)exponent;
+            long tmp = *(long*)&value;
+            long tmp2 = (long)(b_faction * (tmp - 4606921280493453312L)) + 4606921280493453312L;
+            return r * *(double*)&tmp2;
+        }
+    }
+
+    /// <summary>
+    /// A readonly 4x3 double-backed matrix for transform operations
+    /// </summary>
+    public readonly struct TransformMatrix
+    {
+        public readonly double m00, m01, m02, m03;
+        public readonly double m10, m11, m12, m13;
+        public readonly double m20, m21, m22, m23;
+
+        public TransformMatrix(double m00, double m01, double m02, double m03, double m10, double m11, double m12, double m13, double m20, double m21, double m22, double m23)
+        {
+            this.m00 = m00;
+            this.m01 = m01;
+            this.m02 = m02;
+            this.m03 = m03;
+            this.m10 = m10;
+            this.m11 = m11;
+            this.m12 = m12;
+            this.m13 = m13;
+            this.m20 = m20;
+            this.m21 = m21;
+            this.m22 = m22;
+            this.m23 = m23;
+        }
+
+        public TransformMatrix(ref Matrix4x4 transformMatrix)
+        {
+            m00 = transformMatrix.m00;
+            m01 = transformMatrix.m01;
+            m02 = transformMatrix.m02;
+            m03 = transformMatrix.m03;
+
+            m10 = transformMatrix.m10;
+            m11 = transformMatrix.m11;
+            m12 = transformMatrix.m12;
+            m13 = transformMatrix.m13;
+
+            m20 = transformMatrix.m20;
+            m21 = transformMatrix.m21;
+            m22 = transformMatrix.m22;
+            m23 = transformMatrix.m23;
+        }
+
+        public TransformMatrix(ref Matrix4x4D transformMatrix)
+        {
+            m00 = transformMatrix.m00;
+            m01 = transformMatrix.m01;
+            m02 = transformMatrix.m02;
+            m03 = transformMatrix.m03;
+
+            m10 = transformMatrix.m10;
+            m11 = transformMatrix.m11;
+            m12 = transformMatrix.m12;
+            m13 = transformMatrix.m13;
+
+            m20 = transformMatrix.m20;
+            m21 = transformMatrix.m21;
+            m22 = transformMatrix.m22;
+            m23 = transformMatrix.m23;
+        }
+
+        /// <summary>
+        /// Local to world space transform matrix
+        /// </summary>
+        public static TransformMatrix LocalToWorld(Transform transform)
+        {
+            Matrix4x4 matrix = transform.localToWorldMatrix;
+            return new TransformMatrix(
+                matrix.m00, matrix.m01, matrix.m02, matrix.m03,
+                matrix.m10, matrix.m11, matrix.m12, matrix.m13,
+                matrix.m20, matrix.m21, matrix.m22, matrix.m23);
+        }
+
+        /// <summary>
+        /// World to local space (inverse) transform matrix
+        /// </summary>
+        public static TransformMatrix WorldToLocal(Transform transform)
+        {
+            Matrix4x4 matrix = transform.worldToLocalMatrix;
+            return new TransformMatrix(
+                matrix.m00, matrix.m01, matrix.m02, matrix.m03,
+                matrix.m10, matrix.m11, matrix.m12, matrix.m13,
+                matrix.m20, matrix.m21, matrix.m22, matrix.m23);
+        }
+
+        /// <summary>
+        /// Transform point
+        /// </summary>
+        public void MultiplyPoint3x4(ref Vector3d point)
+        {
+            double x = point.x;
+            double y = point.y;
+            double z = point.z;
+            point.x = m00 * x + m01 * y + m02 * z + m03;
+            point.y = m10 * x + m11 * y + m12 * z + m13;
+            point.z = m20 * x + m21 * y + m22 * z + m23;
+        }
+
+        /// <summary>
+        /// Transform point
+        /// </summary>
+        public Vector3d MultiplyPoint3x4(Vector3d point)
+        {
+            return new Vector3d(
+                m00 * point.x + m01 * point.y + m02 * point.z + m03,
+                m10 * point.x + m11 * point.y + m12 * point.z + m13,
+                m20 * point.x + m21 * point.y + m22 * point.z + m23);
+        }
+
+        /// <summary>
+        /// Transform point
+        /// </summary>
+        public Vector3d MultiplyPoint3x4(double x, double y, double z)
+        {
+            return new Vector3d(
+                m00 * x + m01 * y + m02 * z + m03,
+                m10 * x + m11 * y + m12 * z + m13,
+                m20 * x + m21 * y + m22 * z + m23);
+        }
+
+        /// <summary>
+        /// Transform vector
+        /// </summary>
+        public void MultiplyVector(ref Vector3d point)
+        {
+            double x = point.x;
+            double y = point.y;
+            double z = point.z;
+            point.x = m00 * x + m01 * y + m02 * z;
+            point.y = m10 * x + m11 * y + m12 * z;
+            point.z = m20 * x + m21 * y + m22 * z;
+        }
+
+        /// <summary>
+        /// Transform vector
+        /// </summary>
+        public Vector3d MultiplyVector(Vector3d point)
+        {
+            return new Vector3d(
+                m00 * point.x + m01 * point.y + m02 * point.z,
+                m10 * point.x + m11 * point.y + m12 * point.z,
+                m20 * point.x + m21 * point.y + m22 * point.z);
+        }
+
+        /// <summary>
+        /// Transform vector
+        /// </summary>
+        public Vector3d MultiplyVector(double x, double y, double z)
+        {
+            return new Vector3d(
+                m00 * x + m01 * y + m02 * z,
+                m10 * x + m11 * y + m12 * z,
+                m20 * x + m21 * y + m22 * z);
+        }
     }
 }
