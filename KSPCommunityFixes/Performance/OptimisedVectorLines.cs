0334ad53b487e4d262f460da53500d145960e510
Charles Rogers <charlesworthrogers@gmail.com>
2024-11-02 21:30:51 +0000

    Charlie's VectorLine optimisations

--- KSPCommunityFixes/Performance/OptimisedVectorLines.cs
+++ KSPCommunityFixes/Performance/OptimisedVectorLines.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using KSPCommunityFixes.Library;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -20,9 +21,9 @@ namespace KSPCommunityFixes.Performance
             AddPatch(PatchType.Transpiler, typeof(VectorLine), nameof(VectorLine.IntersectAndDoSkip));
 
             AddPatch(PatchType.Transpiler, typeof(VectorLine), nameof(VectorLine.Draw3D));
-
-            // todo: there are more calls that could be patched but I don't know how often they are used.
         }
+
+        #region VectorLine Patches
 
         static IEnumerable<CodeInstruction> VectorLine_Line3D_Transpiler(IEnumerable<CodeInstruction> instructions) =>
             ReplaceWorldToScreenPoint(instructions, 2);
@@ -35,8 +36,6 @@ namespace KSPCommunityFixes.Performance
 
         static IEnumerable<CodeInstruction> VectorLine_Draw3D_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            // todo: Two loops isn't optimal.
-
             instructions = ReplaceWorldToScreenPoint(instructions, 2);
             instructions = ReplaceScreenToWorldPoint(instructions, 4);
 
@@ -99,6 +98,8 @@ namespace KSPCommunityFixes.Performance
 
             return ReplaceCall(instructions, Camera_ScreenToViewportPoint, VectorLineOptimisation_ScreenToViewportPoint, count);
         }
+
+        #endregion
     }
 
     public static class VectorLineCameraProjection
@@ -109,50 +110,55 @@ namespace KSPCommunityFixes.Performance
         public static bool patchEnabled = true;
         public static long lastCachedFrame;
 
-        private static Matrix4x4 worldToClip;
-        private static Matrix4x4 worldToClipInverse;
-
-        // todo: remove.
-        static Matrix4x4 worldToCameraInv;
-        static Matrix4x4 projectionInv;
+        private static TransformMatrix worldToClip;
+        private static TransformMatrix clipToWorld;
 
         // Storing viewport info instead of using Rect properties grants us a few extra frames.
         public struct ViewportInfo
         {
-            public float halfWidth;
-            public float halfHeight;
-            public float width;
-            public float height;
-            public float x;
-            public float y;
+            public double halfWidth;
+            public double halfHeight;
+            public double width;
+            public double height;
+            public double x;
+            public double y;
 
             public ViewportInfo(Rect viewport)
             {
                 width = viewport.width;
                 height = viewport.height;
-                halfWidth = width * 0.5f;
-                halfHeight = height * 0.5f;
+                halfWidth = width * 0.5;
+                halfHeight = height * 0.5;
                 x = viewport.x;
                 y = viewport.y;
             }
         }
 
-        public static ViewportInfo viewport;
+        private static ViewportInfo viewport;
 
         private static void UpdateCache()
         {
+            lastCachedFrame = KSPCommunityFixes.frameCount;
             Camera camera = VectorLine.cam3D;
 
-            worldToClip = camera.projectionMatrix * camera.worldToCameraMatrix;
             viewport = new ViewportInfo(camera.pixelRect);
 
-            worldToCameraInv = camera.worldToCameraMatrix.inverse;
-            projectionInv = camera.projectionMatrix.inverse;
+            Matrix4x4 worldToClip = camera.projectionMatrix * camera.worldToCameraMatrix;
+            VectorLineCameraProjection.worldToClip = new TransformMatrix(ref worldToClip);
 
-            lastCachedFrame = KSPCommunityFixes.frameCount;
+            Matrix4x4 worldToCameraInv = camera.worldToCameraMatrix.inverse;
+            Matrix4x4 projectionInv = camera.projectionMatrix.inverse;
+            projectionInv.m02 += projectionInv.m03;
+            projectionInv.m12 += projectionInv.m13;
+            projectionInv.m22 += projectionInv.m23;
+
+            Matrix4x4 clipToWorld = worldToCameraInv * projectionInv;
+            VectorLineCameraProjection.clipToWorld = new TransformMatrix(clipToWorld.m00, clipToWorld.m01, clipToWorld.m02, camera.worldToCameraMatrix.inverse.m03,
+                                                clipToWorld.m10, clipToWorld.m11, clipToWorld.m12, camera.worldToCameraMatrix.inverse.m13,
+                                                clipToWorld.m20, clipToWorld.m21, clipToWorld.m22, camera.worldToCameraMatrix.inverse.m23);
         }
 
-        // World to clip functions.
+        #region World to Clip
 
         public static Vector3 WorldToScreenPoint(Camera camera, Vector3 worldPosition)
         {
@@ -165,11 +171,17 @@ namespace KSPCommunityFixes.Performance
             if (lastCachedFrame != KSPCommunityFixes.frameCount)
                 UpdateCache();
 
-            Vector3 screen = WorldToClip(ref worldToClip, ref worldPosition);
-            screen.x = viewport.x + (1.0f + screen.x) * viewport.halfWidth;
-            screen.y = viewport.y + (1.0f + screen.y) * viewport.halfHeight;
+            double x = worldPosition.x;
+            double y = worldPosition.y;
+            double z = worldPosition.z;
 
-            return screen;
+            worldToClip.MutateMultiplyPoint3x4(ref x, ref y, ref z);
+
+            double num = 0.5 / z;
+            x = (0.5 + num * x) * viewport.width + viewport.x;
+            y = (0.5 + num * y) * viewport.height + viewport.y;
+
+            return new Vector3((float)x, (float)y, (float)z);
         }
 
         public static Vector3 WorldToViewportPoint(Camera camera, Vector3 worldPosition)
@@ -180,28 +192,22 @@ namespace KSPCommunityFixes.Performance
             if (lastCachedFrame != KSPCommunityFixes.frameCount)
                 UpdateCache();
 
-            Vector3 clip = WorldToClip(ref worldToClip, ref worldPosition);
-            clip.x = (1.0f + clip.x) * 0.5f;
-            clip.y = (1.0f + clip.y) * 0.5f;
+            double x = worldPosition.x;
+            double y = worldPosition.y;
+            double z = worldPosition.z;
 
-            return clip;
+            worldToClip.MutateMultiplyPoint3x4(ref x, ref y, ref z);
+
+            double num = 0.5 / z;
+            x = 0.5 + num * x;
+            y = 0.5 + num * y;
+
+            return new Vector3((float)x, (float)y, (float)z);
         }
 
-        private static Vector3 WorldToClip(ref Matrix4x4 m, ref Vector3 point)
-        {
-            // Skip z and use result.z as w.
+        #endregion
 
-            Vector3 result = default;
-            result.x = m.m00 * point.x + m.m01 * point.y + m.m02 * point.z + m.m03;
-            result.y = m.m10 * point.x + m.m11 * point.y + m.m12 * point.z + m.m13;
-            result.z = m.m30 * point.x + m.m31 * point.y + m.m32 * point.z + m.m33;
-            float num = 1f / result.z;
-            result.x *= num;
-            result.y *= num;
-            return result;
-        }
-
-        // Clip to world functions.
+        #region Clip to World
 
         public static Vector3 ScreenToWorldPoint(Camera camera, Vector3 screenPosition)
         {
@@ -211,42 +217,16 @@ namespace KSPCommunityFixes.Performance
             if (lastCachedFrame != KSPCommunityFixes.frameCount)
                 UpdateCache();
 
-            // A two multiplication world to clip-space conversion, exactly in reverse.
-            // TODO: I would like if this could be done with one multiplication like WorldToScreenPoint, but I don't understand it well enough.
+            double x = screenPosition.x;
+            double y = screenPosition.y;
+            double z = screenPosition.z;
 
-            // Resources:
-            // https://learnopengl.com/Getting-started/Coordinate-Systems
-            // https://www.songho.ca/math/homogeneous/homogeneous.html
-            // https://stackoverflow.com/questions/7692988/opengl-math-projecting-screen-space-to-world-space-coords
+            x = z * ((x - viewport.x) / viewport.halfWidth - 1);
+            y = z * ((y - viewport.y) / viewport.halfHeight - 1);
 
-            // Examples:
-            // https://github.com/OpenSAGE/OpenSAGE/blob/master/src/OpenSage.Game/Graphics/ViewportExtensions.cs#L46
-            // https://github.com/rickomax/psxprev/blob/no-deps/Common/GeomMath.cs#L248
-            // https://github.com/Prograda/Skybolt/blob/b9d4f410ecfe4e2ba1634971a0ecd017815192db/src/Skybolt/SkyboltQt/Viewport/ScreenTransformUtil.cpp#L36
-            // https://github.com/GrognardsFromHell/OpenTemple/blob/master/Core/GFX/NumericsExtensions.cs#L40
+            clipToWorld.MutateMultiplyPoint3x4(ref x, ref y, ref z);
 
-            // It must replicate the behaviour of Camera.ScreenToWorldPoint exactly, including the third component of screenPosition being in world units.
-            // https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html
-
-            // Normalized device coordinates.
-            Vector4 clipSpace = new Vector4()
-            {
-                x = (screenPosition.x - viewport.x) / viewport.width * 2f - 1f,
-                y = (screenPosition.y - viewport.y) / viewport.height * 2f - 1f,
-                z = 1f,
-                w = screenPosition.z,
-            };
-
-            // Perspective multiplication moves it to clip space.
-            clipSpace.x *= clipSpace.w;
-            clipSpace.y *= clipSpace.w;
-            clipSpace.z *= clipSpace.w;
-
-            Vector4 viewSpace = projectionInv * clipSpace;
-            viewSpace.w = 1f;
-            Vector4 worldSpace = worldToCameraInv * viewSpace;
-
-            return worldSpace;
+            return new Vector3((float)x, (float)y, (float)z);
         }
 
         public static Vector3 ScreenToViewportPoint(Camera camera, Vector3 position)
@@ -254,12 +234,13 @@ namespace KSPCommunityFixes.Performance
             //if (!patchEnabled)
             //    return camera.ScreenToViewportPoint(position);
 
-            if (lastCachedFrame != KSPCommunityFixes.frameCount)
-                UpdateCache();
+            //if (lastCachedFrame != KSPCommunityFixes.frameCount)
+            //    UpdateCache();
 
             // Not used by VectorLine.
-
-            return Vector3.zero;
+            throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
