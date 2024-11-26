4bb764fd48698cdbeb50786a8e5d1f6601fec4b1
Lawrence <30965946+Halbann@users.noreply.github.com>
2024-11-26 11:54:20 +0000

    Prevent unnecessary rebuilds and yields

--- KSPCommunityFixes/Performance/CraftBrowserOptimisations.cs
+++ KSPCommunityFixes/Performance/CraftBrowserOptimisations.cs
@@ -3,30 +3,35 @@
 #define DISABLE_THUMBNAIL_LOGGING
 
 using HarmonyLib;
+using KSP.Localization;
 using KSP.UI.Screens;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace KSPCommunityFixes.Performance
 {
     class CraftBrowserOptimisations : BasePatch
     {
-        // These toggles are here for comparison purposes.
-
-        public static bool patchCheckCraftFileType = true;
-
-        public static bool preventImmediateRebuilds = true;
-        public static int lastFrame;
-        public static HashSet<int> builtThisFrame = new HashSet<int>();
-
-        public static bool preventDelayedRebuild = true;
-
         protected override Version VersionMin => new Version(1, 12, 0);
+
+        // These toggles are here for comparison purposes.
+        public static bool patchCheckCraftFileType = true;
+        public static bool preventImmediateRebuilds = true;
+        public static bool preventDelayedRebuild = true;
+        public static bool preventSearchYield = true;
+        public static bool useTimeBudget = true;
+
+        private static int lastBuiltFrame;
+
+        public static float searchKeystrokeDelay = 0.1f;
 
         protected override void ApplyPatches()
         {
@@ -42,6 +47,8 @@ namespace KSPCommunityFixes.Performance
 #if DISABLE_THUMBNAIL_LOGGING
             AddPatch(PatchType.Transpiler, typeof(ShipConstruction), nameof(ShipConstruction.GetThumbnail), new Type[] { typeof(string), typeof(bool), typeof(bool), typeof(FileInfo) });
 #endif
+
+            AddPatch(PatchType.Prefix, typeof(CraftSearch), nameof(CraftSearch.SearchRoutine));
         }
 
         // Fix a miscellanous bug where setbottomButtons ignores showMergeOption. This is useful for Halbann/LazySpawner and BD's Vessel Mover.
@@ -49,7 +56,7 @@ namespace KSPCommunityFixes.Performance
             __instance.btnMerge.gameObject.SetActive(__instance.btnMerge.gameObject.activeSelf && __instance.showMergeOption);
 
         // Speed up CheckCraftFileType by avoiding loading the whole craft file, instead only checking the file path.
-        // Loadmeta could be used here if it turns out that the path does not always contian the facility name.
+        // Loadmeta could be used here if it turns out that the path does not always contain the facility name.
         static bool ShipConstruction_CheckCraftFileType_Prefix(string filePath, ref EditorFacility __result)
         {
             if (!patchCheckCraftFileType)
@@ -87,17 +94,10 @@ namespace KSPCommunityFixes.Performance
             if (!preventImmediateRebuilds)
                 return true;
 
-            if (Time.frameCount != lastFrame)
-            {
-                builtThisFrame.Clear();
-                lastFrame = Time.frameCount;
-            }
-
-            int hash = __instance.GetHashCode();
-            if (builtThisFrame.Contains(hash))
+            if (Time.frameCount == lastBuiltFrame)
                 return false;
 
-            builtThisFrame.Add(hash);
+            lastBuiltFrame = Time.frameCount;
 
             return true;
         }
@@ -111,14 +111,19 @@ namespace KSPCommunityFixes.Performance
             dialog.directoryController.isEnabledThisFrame = false;
         }
 
-        static void CraftBrowserDialog_Start_Postfix(CraftBrowserDialog __instance) => PreventDelayedRebuild(__instance);
+        static void CraftBrowserDialog_Start_Postfix(CraftBrowserDialog __instance)
+        {
+            PreventDelayedRebuild(__instance);
+            CraftSearch.Instance.searchKeystrokeDelay = searchKeystrokeDelay;
+        }
 
-        static void CraftBrowserDialog_ReDisplay_Postfix(CraftBrowserDialog __instance) => PreventDelayedRebuild(__instance);
+        static void CraftBrowserDialog_ReDisplay_Postfix(CraftBrowserDialog __instance) =>
+            PreventDelayedRebuild(__instance);
 
         // Disable the call to Debug.LogFormat when the thumbnail is not found.
+        // On a new save with a large number of imported craft, the game will generate a lot of useless log entries.
         static IEnumerable<CodeInstruction> ShipConstruction_GetThumbnail_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-
             MethodInfo debug = AccessTools.Method(typeof(Debug), nameof(Debug.Log), new Type[] { typeof(object) });
 
             foreach (CodeInstruction instruction in instructions)
@@ -132,6 +137,79 @@ namespace KSPCommunityFixes.Performance
             }
 
             return instructions;
+        }
+
+        // Replace the search routine to prevent the search routine from yielding
+        // a frame on each craft, and to make some small optimisations.
+        static bool CraftSearch_SearchRoutine_Prefix(CraftSearch __instance, ref IEnumerator __result)
+        {
+            if (!preventSearchYield)
+                return true;
+
+            __result = SearchRoutine(__instance);
+            return false;
+        }
+
+        private static IEnumerator SearchRoutine(CraftSearch __instance)
+        {
+            // Delay the start of the search routine by the searchKeystrokeDelay.
+            while (__instance.searchTimer + __instance.searchKeystrokeDelay > Time.realtimeSinceStartup)
+                yield return null;
+
+            bool filtered = false;
+            string searchTerm = __instance.searchField.text;
+            List<CraftEntry> entries = CraftSearch.craftBrowserDialog.craftList;
+            float timer = Time.realtimeSinceStartup;
+            float budget = 1f / 60f / 2f; // 8 ms.
+            bool searchEmpty = string.IsNullOrWhiteSpace(searchTerm);
+
+#if DEBUG
+            Stopwatch stopwatch = Stopwatch.StartNew();
+#endif
+
+            foreach (CraftEntry entry in entries)
+            {
+                bool result = searchEmpty || FasterCraftMatchesSearch(entry, searchTerm);
+
+                if (entry.gameObject.activeSelf != result)
+                    entry.gameObject.SetActive(result);
+
+                if (!filtered && !result)
+                    filtered = true;
+
+                if (useTimeBudget && Time.realtimeSinceStartup - timer > budget)
+                {
+                    timer = Time.realtimeSinceStartup;
+                    yield return null;
+                }
+            }
+
+#if DEBUG
+            stopwatch.Stop();
+            Debug.Log($"[CraftBrowserOptimisations]: Filtered {entries.Count} craft in {stopwatch.Elapsed.Milliseconds:N3} ms.");
+#endif
+
+            __instance.hasFiltered?.Invoke(filtered);
+
+            if (string.IsNullOrWhiteSpace(searchTerm) && __instance.IsDifferentSearch)
+                __instance.StopSearch();
+
+            __instance.previousSearch = searchTerm;
+        }
+
+        private static bool FasterCraftMatchesSearch(CraftEntry craft, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return true;
+
+            if (craft.craftName.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) > -1)
+                return true;
+
+            if (craft.craftProfileInfo == null || string.IsNullOrEmpty(craft.craftProfileInfo.description))
+                return false;
+
+            string text = Localizer.Format(craft.craftProfileInfo.description);
+            return text.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) > -1;
         }
     }
 }
