b8ecc627294c3ec3f9642330c43ac2f4e7e09d93
gotmachine <24925209+gotmachine@users.noreply.github.com>
2025-03-08 16:35:05 +0100

    Actually better throttling mechanism, with both an upper in-game time limit and a real world time minimum interval.

--- KSPCommunityFixes/Performance/CommNetThrottling.cs
+++ KSPCommunityFixes/Performance/CommNetThrottling.cs
@@ -1,31 +1,43 @@
-﻿namespace KSPCommunityFixes.Performance
+﻿using System.Diagnostics;
+
+namespace KSPCommunityFixes.Performance
 {
     class CommNetThrottling : BasePatch
     {
-        private static double updateInterval = 2.5;
-        private static long lastUpdateFixedFrame = 0L;
+        private static double maxGameTimeInterval = 2.5;
+        private static long minRealTimeInterval = 20;
+        private static Stopwatch realTimeUpdateTimer = new Stopwatch();
 
         protected override void ApplyPatches()
         {
-            KSPCommunityFixes.SettingsNode.TryGetValue("CommNetThrottlingUpdateInterval", ref updateInterval);
+            ConfigNode settingsNode = KSPCommunityFixes.SettingsNode.GetNode("COMMNET_THROTTLING_SETTINGS");
+
+            if (settingsNode != null)
+            {
+                settingsNode.TryGetValue("maxGameTimeInterval", ref maxGameTimeInterval);
+                settingsNode.TryGetValue("minRealTimeInterval", ref minRealTimeInterval);
+            }
+
             AddPatch(PatchType.Override, typeof(CommNet.CommNetNetwork), nameof(CommNet.CommNetNetwork.Update));
         }
 
         static void CommNetNetwork_Update_Override(CommNet.CommNetNetwork commNetNetwork)
         {
-            double currentTime = Planetarium.GetUniversalTime();
-            double interval = currentTime - commNetNetwork.prevUpdate;
+            double currentGameTime = Planetarium.GetUniversalTime();
+            double currentGameTimeInterval = currentGameTime - commNetNetwork.prevUpdate;
             if (!commNetNetwork.queueRebuild && !commNetNetwork.commNet.IsDirty 
-                && interval >= 0.0
-                && (interval < updateInterval || KSPCommunityFixes.FixedUpdateCount == lastUpdateFixedFrame))
+                && currentGameTimeInterval >= 0.0
+                && (currentGameTimeInterval < maxGameTimeInterval || realTimeUpdateTimer.ElapsedMilliseconds < minRealTimeInterval))
             {
                 commNetNetwork.graphDirty = true;
                 return;
             }
 
-            lastUpdateFixedFrame = KSPCommunityFixes.FixedUpdateCount;
+            // UnityEngine.Debug.Log($"CommNet update interval : {realTimeUpdateTimer.Elapsed.TotalMilliseconds:F0}ms");
+
             commNetNetwork.commNet.Rebuild();
-            commNetNetwork.prevUpdate = currentTime;
+            realTimeUpdateTimer.Restart();
+            commNetNetwork.prevUpdate = currentGameTime;
             commNetNetwork.graphDirty = false;
             commNetNetwork.queueRebuild = false;
         }
