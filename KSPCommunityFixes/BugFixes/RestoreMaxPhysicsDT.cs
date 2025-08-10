fc19e3b57d3aa3f3f92652af2d565cc8d0412e73
gotmachine <24925209+gotmachine@users.noreply.github.com>
2025-08-10 09:40:05 +0200

    Fixed the RestoreMaxPhysicsDT patch not being 100% reliable The previous implementation was attempting to restore `Time.maximumDeltaTime` in a prefix, before stock was setting `Time.fixedDeltaTime`. Since the minimum value of `maximumDeltaTime` was clamped to the (not yet changed) `fixedDeltaTime` value, it was possible for a higher value than `GameSettings.PHYSICS_FRAME_DT_LIMIT` to end up being set. In fact, the previous implementation only worked because `fixedDeltaTime` changes are (usually but not always) lerped over multiple frames. The issue was notably visible when having `PHYSICS_FRAME_DT_LIMIT` set to 0.02, in which case the value was always set to the lerped value from the last frame. Implementing the proper logic in a postfix fixes that issue.

--- KSPCommunityFixes/BugFixes/RestoreMaxPhysicsDT.cs
+++ KSPCommunityFixes/BugFixes/RestoreMaxPhysicsDT.cs
@@ -1,9 +1,6 @@
-﻿using HarmonyLib;
+﻿//#define DEBUG_PHYSICS_DT
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace KSPCommunityFixes.BugFixes
@@ -14,16 +11,34 @@ namespace KSPCommunityFixes.BugFixes
 
         protected override void ApplyPatches()
         {
-            AddPatch(PatchType.Prefix, typeof(TimeWarp), "updateRate");
+            AddPatch(PatchType.Postfix, typeof(TimeWarp), nameof(TimeWarp.updateRate));
         }
 
-        static void TimeWarp_updateRate_Prefix()
+        // when you use physics warp, it increases Time.fixedDeltaTime
+        // Unity will internally increase maximumDeltaTime to be at least as high as fixedDeltaTime
+        // But nothing in the KSP code will ever return maximumDeltaTime to the value from the settings when time warp is over
+        static void TimeWarp_updateRate_Postfix()
         {
-            // when you use physics warp, it increases Time.fixedDeltaTime
-            // Unity will internally increase maximumDeltaTime to be at least as high as fixedDeltaTime
-            // But nothing in the KSP code will ever return maximumDeltaTime to the value from the settings when time warp is over
-            // Setting it just before TimeWarp sets fixedDeltaTime guarantees that all points of code will have the correct value
-            Time.maximumDeltaTime = GameSettings.PHYSICS_FRAME_DT_LIMIT;
+            float fixedDT = Time.fixedDeltaTime;
+            if (Time.maximumDeltaTime > fixedDT)
+            {
+                if (fixedDT > GameSettings.PHYSICS_FRAME_DT_LIMIT)
+                    Time.maximumDeltaTime = fixedDT;
+                else
+                    Time.maximumDeltaTime = GameSettings.PHYSICS_FRAME_DT_LIMIT;
+            }
         }
     }
+
+#if DEBUG_PHYSICS_DT
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class PhysicsDTDisplay : MonoBehaviour
+    {
+        void OnGUI()
+        {
+            GUI.Label(new Rect(0, 100, 200, 20), $"DT    : {Time.fixedDeltaTime}");
+            GUI.Label(new Rect(0, 120, 200, 20), $"MaxDT : {Time.maximumDeltaTime}");
+        }
+    }
+#endif
 }
