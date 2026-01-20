fbbe476c95a9c3e3d7311feb7c575d225d3c9ab1
BattlePants74 <45367549+MajorNr01@users.noreply.github.com>
2026-01-20 22:41:42 +0100

    Fix lower limit for wheel moment of inertia (#341)
    When the game calculates the moment of inertia for wheels, it sets a hard-coded lower limit of `0.01`. This is much larger than the calculated moment of inertia of most landing gear wheels (e.g. the LY-10 Small Landing Gear has a moment of inertia of `0.000648`).
    
    This causes significant forces on small aircraft (~10t) during touchdown as the wheels spin up. As a result, the craft pitches down if the wheels are not spinning and pitches up if the wheels are still spinning. This is correct in principle but very exaggerated in small aircraft due to the inappropriately large moment of inertia.
    
    This patch changes the lower limit to `0.00001` which is lower than the correct moment of inertia of the stock landing gears.
    
    The chosen lower limit is kind of arbitrary (I just added three zeroes) and I am completely open to lowering it even further or removing it entirely.

---
+++ KSPCommunityFixes/BugFixes/WheelInertiaLimit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using VehiclePhysics;
+using UnityEngine;
+
+namespace KSPCommunityFixes
+{
+    public class WheelInertiaLimit : BasePatch
+    {
+        protected override Version VersionMin => new Version(1, 12, 0);
+
+        protected override void ApplyPatches()
+        {
+            AddPatch(PatchType.Postfix, typeof(VehiclePhysics.Wheel), nameof(VehiclePhysics.Wheel.RecalculateConstants));
+        }
+
+        static void Wheel_RecalculateConstants_Postfix(VehiclePhysics.Wheel __instance)
+        {
+            __instance.I = __instance.mass * __instance.radius * __instance.radius * 0.5f;
+
+            if (__instance.I < 0.00001f)
+            {
+                __instance.I = 0.00001f;
+            }
+
+            __instance.invI = 1f / __instance.I;
+        }
+    }
+}
