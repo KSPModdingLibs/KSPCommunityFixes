3c1c97d09659dab6423d9128e6b475399dd2221f
siimav <1120038+siimav@users.noreply.github.com>
2026-04-10 04:09:16 +0300

    Fix mouse double clicks passing through to parts on vessel while the the map view is open

---
+++ KSPCommunityFixes/BugFixes/BlockMapViewPartClick.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KSPCommunityFixes
+{
+    public class BlockMapViewPartClick : BasePatch
+    {
+        protected override Version VersionMin => new Version(1, 12, 0);
+
+        protected override void ApplyPatches()
+        {
+            AddPatch(PatchType.Prefix, typeof(Part), "UpdateMouseOver");
+        }
+
+        static bool Part_UpdateMouseOver_Prefix()
+        {
+            return !HighLogic.LoadedSceneIsFlight || CameraManager.Instance.currentCameraMode != CameraManager.CameraMode.Map;
+        }
+    }
+}
