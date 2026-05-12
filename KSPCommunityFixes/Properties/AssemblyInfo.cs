ac2e4e677da201a133bb64d84e445c119000de59
Phantomical <phantom@lynches.ca>
2026-05-11 18:53:51 -0700

    Migrate to KSPBuildTools

--- KSPCommunityFixes/Properties/AssemblyInfo.cs
+++ KSPCommunityFixes/Properties/AssemblyInfo.cs
@@ -1,38 +1,4 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("KSPCommunityFixes")]
-[assembly: AssemblyDescription("MIT license")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KSPCommunityFixes")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4e405c02-5aeb-4975-b26c-07582bb3fb15")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.40.0.0")]
-[assembly: AssemblyFileVersion("1.40.1.0")]
-
-[assembly: KSPAssembly("KSPCommunityFixes", 1, 40, 1)]
-[assembly: KSPAssemblyDependency("MultipleModulePartAPI", 1, 0, 0)]
-[assembly: KSPAssemblyDependency("ModuleManager", 1, 0)]
-[assembly: KSPAssemblyDependency("HarmonyKSP", 1, 0)]
